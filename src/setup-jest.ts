6a6ddd72dad3631826a7dec18d085429d86a59ae
Rennan Rodrigues <martini.rennan@gmail.com>
2024-08-31 19:24:49 -0300

    Initial commit

---
+++ src/setup-jest.ts
@@ -0,0 +1,7 @@
+import 'jest-preset-angular/setup-jest';
+
+/**
+ * This file is used to setup jest for Angular.
+ * It only was possible with the help of below link:
+ * [https://medium.com/@philip.mutua/setting-up-jest-in-your-angular-16-project-3638ef65f3a3]
+*/
